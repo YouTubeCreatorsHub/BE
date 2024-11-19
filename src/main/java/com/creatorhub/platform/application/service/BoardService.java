db2d8a62bfb1bbee3b2d428be0487757899dafac
rake_13@naver.com <wltjq1203@icloud.com>
2024-11-19 21:53:40 +1100

    BoardService작성

---
+++ src/main/java/com/creatorhub/platform/application/service/BoardService.java
@@ -0,0 +1,36 @@
+package com.creatorhub.platform.application.service;
+
+import com.creatorhub.platform.domain.entity.Board;
+import com.creatorhub.platform.domain.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public Board createBoard(String name) {
+        return boardRepository.save(
+                Board.builder()
+                        .name(name)
+                        .build()
+        );
+    }
+
+    public Board retrieveBoard(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(NoSuchElementException::new);
+    }
+
+    public  Board updateBoardName(Board board, String newName){
+        Board renamed = board.rename(newName);
+        return boardRepository.save(renamed);
+    }
+
+    public Board deleteBoard(Board board){
+        boardRepository.delete(board);
+        return board;
+    }
+}
