def10aa5d9d5d0b688cf9d5ded44c3f85ac3ee32
rake_13@naver.com <wltjq1203@icloud.com>
2024-11-19 21:49:58 +1100

    ArticleService작성

---
+++ src/main/java/com/creatorhub/platform/application/service/ArticleService.java
@@ -0,0 +1,62 @@
+package com.creatorhub.platform.application.service;
+
+import com.creatorhub.platform.domain.entity.Article;
+import com.creatorhub.platform.domain.entity.Board;
+import com.creatorhub.platform.domain.entity.Category;
+import com.creatorhub.platform.domain.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public Article createArticle(String title, String content, Board board, Category categor) {
+        return articleRepository.save(
+                Article.builder()
+                        .title(title)
+                        .content(content)
+                        .board(board)
+                        .category(categor)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+    }
+
+    public Article getArticle(Long articleId) {
+        return articleRepository.findById(articleId).orElseThrow(NoSuchElementException::new);
+    }
+
+    public Page<Article> getArticles(Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return articleRepository.findAll(pageable);
+    }
+
+    public Article updateArticleTitle(Article article, String title) {
+        return articleRepository.save(article.updateTitle(title));
+    }
+
+    public Article updateArticleContent(Article article, String content) {
+        return articleRepository.save(article.updateContent(content));
+    }
+
+    public Article updateArticleCategory(Article article, Category category) {
+        return articleRepository.save(article.updateCategory(category));
+    }
+
+    public Article updateArticle(Article article, Board board) {
+        return articleRepository.save(article.updateBoard(board));
+    }
+
+    public Article deleteArticle(Article article) {
+        articleRepository.delete(article);
+        return article;
+    }
+}
